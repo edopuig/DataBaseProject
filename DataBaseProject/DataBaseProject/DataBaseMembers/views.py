357eef250f85e33e6cc9768f5f3f257da2fa7ee8
Edo <edopuig@gmail.com>
2022-08-27 13:32:59 +0200

    Primera Tabla en la app

--- DataBaseProject/DataBaseProject/DataBaseMembers/views.py
+++ DataBaseProject/DataBaseProject/DataBaseMembers/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def index(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
